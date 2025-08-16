5daf3a0f758536b96af4f0b80c2bbf1a905b6250
Tokuue Masae <toccocco@toccos-MacBook-Air.local>
2025-08-16 16:07:27 +0900

    Updated cute design and natural voice

---
+++ sw.js
@@ -0,0 +1,26 @@
+const CACHE = "cheer-workout-v6";
+const ASSETS = [
+  "./",
+  "./index.html",
+  "./manifest.json",
+  "./icon-192.png",
+  "./icon-512.png"
+];
+
+self.addEventListener("install", (e) => {
+  e.waitUntil(caches.open(CACHE).then((c) => c.addAll(ASSETS)));
+});
+
+self.addEventListener("activate", (e) => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    )
+  );
+});
+
+self.addEventListener("fetch", (e) => {
+  e.respondWith(
+    caches.match(e.request).then(res => res || fetch(e.request))
+  );
+});
